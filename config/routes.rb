c1751a2ddb0c078d98847874f0f1901aac8c1aff
wazuo <95359918+wazuo@users.noreply.github.com>
2022-02-09 08:33:41 +0000

    [add] ログイン機能実装　member新規登録はまだ未完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # 会員用
+  # URL /members/sign_in...
+  devise_for :members,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in...
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
 end
