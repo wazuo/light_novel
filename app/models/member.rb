cfefd6d96b22cb45bf7060d3e97c75fa33c27c78
wazuo <95359918+wazuo@users.noreply.github.com>
2022-02-15 02:56:47 +0000

    [add] フォロー機能完成！

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,13 +7,28 @@ class Member < ApplicationRecord
   has_many :mangas, dependent: :destroy
   has_many :manga_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id",dependent: :destroy
   # 自分がフォロー（与フォロー）側の関係性
-  has_many :relationships, class_name: "Relationship", foreign_key: "followers_id",dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id",dependent: :destroy
   # 被フォロー関係を通じて参照→自分をフォローしている人
-  has_many :followers, through: :reverse_of_relationships ,source: :follower
-  与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 与フォロー関係を通じて参照→自分がフォローしている人
   has_many :followings,through: :relationships, source: :followed
+
+  # 会員のフォローのメソッド定義
+  def follow(member_id)
+     relationships.create(followed_id: member_id)
+    #relationship = relationships.new(followed_id: member_id)
+    #relationship.save!
+  end
+  # 会員のフォローを外すメソッド定義
+  def unfollow(member_id)
+    relationships.find_by(followed_id: member_id).destroy
+  end
+  # 会員がフォローしているユーザーの一覧を表すメソッド定義
+  def following?(member)
+    followings.include?(member)
+  end
 end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,18 +1,7 @@
 class Relationship < ApplicationRecord
   # フォローする会員
-  belongs_to :follwer, class_name: "Member"
+  belongs_to :follower, class_name: "Member"
   # フォローされる会員のid
-  belongs_to :follwed, class_name: "Member"
-# 会員のフォローのメソッド定義
-  def follow(member_id)
-    relationships.create(followed_id: member_id)
-  end
-  # 会員のフォローを外すメソッド定義
-  def unfollow(member_id)
-    relationships.find_by(followed_id: member_id).destroy
-  end
-  # 会員がフォローしているユーザーの一覧を表すメソッド定義
-  def following?(member)
-    followings.include?(member)
-  end
+  belongs_to :followed, class_name: "Member"
+
 end
