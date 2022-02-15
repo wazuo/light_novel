18fb706700d515c37bbcd69178a656fa0a751687
wazuo <95359918+wazuo@users.noreply.github.com>
2022-02-15 04:42:07 +0000

    [add]searchコントローラー作成＆定義完了

---
+++ app/controllers/public/searches_controller.rb
@@ -0,0 +1,22 @@
+class Public::SearchesController < ApplicationController
+  # 検索機能のコントローラー
+  def search
+    # viewのform_tagにて選択したmodelの値を@modelに代入。
+    @model = params[:model]
+    # 検索ワードを@contentに代入。
+    @content = params[:content]
+    # 選択した検索方法の値を@methodに代入。
+    @method = params[:method]
+    # @model, @content, @methodを代入した、search_forを@recordsに代入。
+    @records = search_for(@model, @content,@method)
+    # 選択したモデルがmemberだったら
+    if @model == 'member'
+      # search_forを@recordsに代入。
+      @records = Member.search_for(@content,@method)
+      # modelでなかったら
+    else
+      # search_forを@recordsに代入。
+      @records = Manga.search_for(@content,@method)
+    end
+  end
+end
