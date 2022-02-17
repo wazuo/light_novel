3a4d3d625695207c4e5a01000ec05865a791d46e
wazuo <95359918+wazuo@users.noreply.github.com>
2022-02-17 03:56:09 +0000

    [add]非同期通信いいね完了

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,17 +4,15 @@ class Public::FavoritesController < ApplicationController
 
   # いいね作成
   def create
-    manga = Manga.find(params[:manga_id])
-    favorite = current_member.favorites.new(manga_id: manga.id)
+    @manga = Manga.find(params[:manga_id])
+    favorite = current_member.favorites.new(manga_id: @manga.id)
     favorite.save
-    redirect_to manga_path(manga)
   end
 
   # いいね削除（解除）
   def destroy
-    manga = Manga.find(params[:manga_id])
-    favorite = current_member.favorites.find_by(manga_id: manga.id)
+    @manga = Manga.find(params[:manga_id])
+    favorite = current_member.favorites.find_by(manga_id: @manga.id)
     favorite.destroy
-    redirect_to manga_path(manga)
   end
 end
