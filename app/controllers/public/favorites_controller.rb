4cbc34891dfdd357befcc7365693a053f18e9c8f
wazuo <95359918+wazuo@users.noreply.github.com>
2022-02-17 12:35:48 +0000

    コメント非同期通信化達成

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -7,6 +7,7 @@ class Public::FavoritesController < ApplicationController
     @manga = Manga.find(params[:manga_id])
     favorite = current_member.favorites.new(manga_id: @manga.id)
     favorite.save
+    # 非同期化のためredirect_toは削除
   end
 
   # いいね削除（解除）
@@ -14,5 +15,6 @@ class Public::FavoritesController < ApplicationController
     @manga = Manga.find(params[:manga_id])
     favorite = current_member.favorites.find_by(manga_id: @manga.id)
     favorite.destroy
+    # 非同期化のためredirect_toは削除
   end
 end
--- app/controllers/public/manga_comments_controller.rb
+++ app/controllers/public/manga_comments_controller.rb
@@ -4,20 +4,23 @@ class Public::MangaCommentsController < ApplicationController
   # 投稿に対するコメントの作成
   def create
     # 投稿するためのマンガのidの習得
-    manga = Manga.find(params[:manga_id])
+    @manga = Manga.find(params[:manga_id])
     # 会員のidの習得とフォームに渡す空のデータの値を設定
-    comment = current_member.manga_comments.new(manga_comment_params)
+    @comment = current_member.manga_comments.new(manga_comment_params)
     # マンガのidを習得
-    comment.manga_id = manga.id
-    comment.save
-    redirect_to request.referer
+    @comment.manga_id = @manga.id
+    @comment.save
+    # 非同期化のためredirect_toは削除
+    render :index
   end
 
   # コメント投稿の削除
   def destroy
     # 削除するコメントのidを習得し削除する
     MangaComment.find_by(id:params[:id]).destroy
-    redirect_to request.referer
+    @manga = Manga.find(params[:manga_id])
+    # 非同期化のためredirect_toは削除
+    render :index
   end
 
   private
