109ce295a706a8695a6b3e48c4936ef83cae99f9
wazuo <95359918+wazuo@users.noreply.github.com>
2022-02-20 15:41:03 +0000

    [add] お問い合わせ機能実装

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -18,7 +18,7 @@ class Public::ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(contact_params)
-    if @contact.save!
+    if @contact.save
       ContactMailer.send_mail(@contact).deliver_now
       redirect_to done_path
     else
